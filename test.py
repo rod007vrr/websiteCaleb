9a5a2392abadabdb9d8aba980b380a11ed92315f
rod007vrr <rod007vrr@gmail.com>
2020-11-11 21:03:11 -0500

    done

--- test.py
+++ test.py
@@ -1,36 +1,7 @@
-import numpy as np
 
-a = np.array([1,2,3])
+from pprint import pprint
 
-print(a)
+my_dict = {'name': 'Yasoob', 'age': 'undefined', 'personality': 'awesome'}
+# ['__add__', '__class__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__gt__', '__hash__', '__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
 
-x = np.arange(4).reshape(2,2)
-print(x)
-
-y = np.zeros((5,6),dtype=int)
-print(y)
-
-number = y[1]
-
-print(number)
-
-y[1,2] = 100
-
-print(y[1,2])
-
-
-
-'''
-values = [[0,0],[0,0]]
-values[0][0] = 1
-values[0][1] = 1
-print(values[0])
-if values[0] == [1,1]:
-    print("yes")
-
-
-
-rows, cols = (5, 5) 
-arr = [[0]*cols]*rows 
-print(arr) 
-'''
+print(my_dict)

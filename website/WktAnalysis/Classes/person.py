80efdf7e08e2d072c13e963ef4f812125a939631
rod007vrr <rod007vrr@gmail.com>
2021-01-10 23:14:31 -0500

    did a lot

--- website/WktAnalysis/Classes/person.py
+++ website/WktAnalysis/Classes/person.py
@@ -6,26 +6,21 @@ class Person:
             
         def insert(self, node):
             self.subNodes.append(node)
-     
-    class Exercise(object):
-        def __init__(self, name, targetedIntensity):
-            self.name = name
-            self.targetedIntensity = targetedIntensity
-        def print(self):
-            print(f"\n{self.name}", end="")
-            for n in self.targetedIntensity.items():
-                print(n) 
-     
+            
+        def printTree(self):
+            print(self.data)
+            for n in self.subNodes:
+                n.printTree() 
                 
-    def __init__(self, UUID):
+    def __init__(self, UUID): #! Change this function to take the paths to the exercises as inputs
         self.UUID = UUID
         
         #Constructing body tree
         with open(r"body.txt", 'r') as file: #!change filename
             data = [str(line.replace("    ", "?")).strip() for line in file]
-        bodyTree = Person.Node({data[0]: 0})
+        self.bodyTree = Person.Node({data[0]: 0})
         del data[0]
-        currentRoot = bodyTree
+        currentRoot = self.bodyTree
         for part in data:
             if part.count("?") > list(currentRoot.data.keys())[0].count("?"):
                 currentRoot.insert(Person.Node({part.strip("?"): 0}))
@@ -34,17 +29,54 @@ class Person:
         
         self.stats = currentRoot
         
-    def updateStats(workout):
-        """Updates stats with workout
+        #Constructing exercises
+        with open(r"exercises.txt", 'r') as file: #!change filename
+            data = [line for line in file]
+            
+        self.exercises = {}
+        tempName = ""
+        tempPairs = {}
+        
+        for n in data:
+            if n == "\n":
+                self.exercises[tempName[:-2]] = tempPairs
+                tempName = ""
+                tempPairs = {}
+            elif not n.startswith("    "):
+                tempName = n.strip(":")
+            else:
+                pair = n.split(":")
+                tempPairs[pair[0].strip()] = int(pair[1].strip())
 
-        Args:
-            workout ([string]): [file path to workout.txt]
-        """
+    def updateStats(self, workout):
+        
+        
         def processExercise(exercise, reps):
-            for n in exercise:
+            for bpName, bpAmount in self.exercises[exercise].items():
+                updateSpec(self.bodyTree ,bpName, bpAmount*reps)
+                
+        def updateSpec(source ,name, amount):
+
+            
+            try:
+                source.data[name] += amount
+            except:
                 pass
             
-        
+            for n in source.subNodes:
+                try:
+                    n.data[name] += amount
+                    for x in n.subNodes:
+                        x.data
+                        
+                except:
+                    pass
+                
+                        
+                
+                
+            
+            
         
         with open(workout, 'r') as file:
             data = [line for line in file]
@@ -52,7 +84,4 @@ class Person:
         for n in data:
             splitD = n.split(":")
             processExercise(splitD[0].strip(), int(splitD[1]))
-            
-            
-            
-            
+   
--- website/WktAnalysis/main.py
+++ website/WktAnalysis/main.py
@@ -1,23 +1,6 @@
-from Classes.exercise import Exercise
 from Classes.person import Person
 
-#Constructing exercises
-with open(r"exercises.txt", 'r') as file: #!change filename
-    data = [line for line in file]
-    
-exercises = []
 
-currentExerciseName = ""
-currentExerciseTargetIntensityPairs = {}
-
-for n in data:
-    if n == "\n":
-        exercises.append(Exercise(currentExerciseName, currentExerciseTargetIntensityPairs))
-    elif not n.startswith("    "):
-        currentExerciseName = n.strip(":")
-    else:
-        pair = n.split(":")
-        currentExerciseTargetIntensityPairs[pair[0].strip()] = int(pair[1].strip())
 
 #Testing for individual person
 
@@ -25,4 +8,6 @@ test = Person(0)
 
 test.stats.printTree()
 
-test.updateStats()
+test.updateStats("workout.txt")
+
+test.stats.printTree()
